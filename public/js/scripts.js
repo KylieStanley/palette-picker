661fed3b0793d174ac58092c8cb12de1cf4427c9
Kylie Stanley <knstanley11@gmail.com>
2019-01-22 20:49:30 -0700

    Add function for toggling locked colors

--- public/js/scripts.js
+++ public/js/scripts.js
@@ -1,4 +1,6 @@
 const colors = document.querySelectorAll('.color')
+const generateColorsBtn = document.querySelector('.generate-colors')
+const paletteContainer = document.querySelector('.palette')
 
 const generateColorPalette = () => {
   const values = '0123456789ABCDEF'
@@ -10,8 +12,24 @@ const generateColorPalette = () => {
       color += values[(Math.floor(Math.random() * 16))]
     }
 
-    item.style.backgroundColor = color
+    if (item.childNodes[1].className !== 'fas fa-lock') {
+      item.style.backgroundColor = color
+      item.childNodes[3].innerText = color 
+    }
   })
 }
+
+const toggleLock = (e) => {
+  if (e.target.className === 'fas fa-lock-open') {
+    e.target.className = 'fas fa-lock'
+  } else if (e.target.className === 'fas fa-lock') {
+    e.target.className = 'fas fa-lock-open'
+  }
+}
+
+
+generateColorsBtn.addEventListener('click', generateColorPalette)
+paletteContainer.addEventListener('click', toggleLock)
+
 
 generateColorPalette()
