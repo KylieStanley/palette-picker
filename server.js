ef7c08c8906855db4296c997cc79ee919fa1a5fd
Kylie Stanley <knstanley11@gmail.com>
2019-01-22 22:13:45 -0700

    Add endpoints to server.js

--- server.js
+++ server.js
@@ -1,8 +1,17 @@
 const express = require('express');
 const app = express();
+const bodyParser = require('body-parser');
 
-app.use(express.static('public'))
+app.use(bodyParser.json());
+app.use(express.static('public'));
 
 app.listen(3000, () => {
   console.log('Palette Picker running on localhost:3000');
+});
+
+app.locals.palettes = []
+
+app.get('/api/v1/palettes', (request, response) => {
+  const palettes = app.locals.palettes;
+  return response.json({ palettes });
 });
